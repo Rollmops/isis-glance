de709b41d98cec05d6fa603fa52ff3391d6f01e8
Erik Türke <tuerke@cbs.mpg.de>
2012-11-21 13:41:30 +0100

    add benchmark macros

--- tests/benchmarks/benchmark.hpp
+++ tests/benchmarks/benchmark.hpp
@@ -3,6 +3,9 @@
 
 #include <ctime>
 #include <iostream>
+#include <string>
+#include <list>
+#include <boost/foreach.hpp>
 
 #define START_TIMER() \
 		clock_t timeStart = clock();
@@ -13,15 +16,55 @@
 		"\": " << static_cast<double>(timeEnd - timeStart) \
 		/ CLOCKS_PER_SEC * 1000.0 << "ms" << std::endl;
 
+
 namespace isis {
 namespace glance {
 namespace benchmark {
 
-typedef uint32_t IterType;
+typedef uint16_t IterType;
 const IterType iterations = std::numeric_limits<IterType>::max();
 
+namespace _internal {
+class BenchmarkBase {
+public:
+	virtual void operator ()() const = 0;
+	virtual ~BenchmarkBase() {}
+	std::string name;
+};
+
+std::list<BenchmarkBase*> benchmarkList;
+
+} // end namespace _internal
 }
 }
 }
+#define DEFINE_BENCHMARK(NAME) \
+	class Benchmark_ ##NAME \
+	: public isis::glance::benchmark::_internal::BenchmarkBase { \
+	public: \
+		Benchmark_ ##NAME() { \
+			isis::glance::benchmark::_internal::benchmarkList.push_back( this ); \
+			name = std::string( "" #NAME "");\
+		} \
+		virtual void operator()() const; \
+	}; \
+	static const Benchmark_ ##NAME  _instance_ ##NAME; \
+	void Benchmark_ ##NAME ::operator() () const
+
+
+#define RUN_BENCHMARKS() \
+	int main() { \
+	using namespace isis::glance::benchmark; \
+	typedef std::list<_internal::BenchmarkBase*> ListType; \
+	BOOST_FOREACH( ListType::const_reference ref, _internal::benchmarkList) \
+	{ \
+		START_TIMER() \
+		for( IterType i = 0; i < iterations; ++i) { \
+			ref->operator()(); \
+		}\
+		END_TIMER(ref->name) \
+	}\
+	return 0; }
+
 
 #endif // _ISIS_GLANCE_BENCHMARK_HPP
--- tests/benchmarks/orientationStressTest.cpp
+++ tests/benchmarks/orientationStressTest.cpp
@@ -1,15 +1,14 @@
 #include "benchmark.hpp"
 #include <util/orientation.hpp>
+#include <iostream>
 
-int main()
+isis::glance::util::Orientation orientation;
+bool isLatched;
+
+DEFINE_BENCHMARK(test1)
 {
-	using namespace isis::glance::benchmark;
-	isis::glance::util::Orientation orientation;
-	bool isLatched;
-	START_TIMER()
-	for( IterType i = 0; i<iterations; ++i ) {
-		isLatched = orientation.isLatched();
-	}
-	END_TIMER("isis::glance::Orientation::isLatched()")
-	return 0;
+	isLatched = orientation.isLatched();
 }
+
+
+RUN_BENCHMARKS()
